77951d6be195fbaf8d5ba0aa939d1251b010d44a
ivanpastor24 <ipastorprats@gmail.com>
2024-12-26 18:43:06 +0100

    Directorio GitHub PowerShell actualizado

---
+++ unidad_6-powershell/practica_2/ejercicio3.ps1
@@ -0,0 +1,6 @@
+﻿[int]$horas_trabajadas = Read-Host "Introduzca el número de horas trabajadas"
+[int]$coste_hora = Read-Host "Introduzca el coste por hora"
+
+$salario = $horas_trabajadas * $coste_hora
+
+Write-Host "El salario que debemos pagar al trabajador es de $salario €."
