040bc62fa3bf2094bcf8e841bb06e816b1104e28
ivanpastor24 <ipastorprats@gmail.com>
2025-01-10 11:58:43 +0100

    Ejercicios powershell

---
+++ unidad_6-powershell/practica_4/ejercicio6.ps1
@@ -0,0 +1,18 @@
+﻿[int]$numero_introducido = Read-Host "Introduzca el número a adivinar"
+
+$numero_acertado = 24
+
+while ($numero_introducido -ne $numero_acertado)
+{
+ if ($numero_introducido -lt $numero_acertado) {
+     Write-Host "El número introducido es menor al número a adivinar."
+     [int]$numero_introducido = Read-Host "Introduzca el número a adivinar"
+ } elseif ($numero_introducido -gt $numero_acertado) {
+     Write-Host "El número introducido es mayor al número a adivinar."
+     [int]$numero_introducido = Read-Host "Introduzca el número a adivinar"
+ }
+}
+
+if ($numero_introducido -eq $numero_acertado) {
+    Write-Host "¡Enhorabuena! Usted ha acertado el número."
+}
