7a3b04bbf3137714974b1ed71fd4df40d48e6862
ivanpastor24 <ipastorprats@gmail.com>
2025-02-01 04:00:02 +0100

    Ejercicios powershell

---
+++ unidad_8-administracion_sistemas_windows/Procesos y servicios/ejercicio3.ps1
@@ -0,0 +1,5 @@
+﻿$usuario = Read-Host "Introduzca un nombre de usuario (Ejemplo: EMPRESA\Usuario)"
+
+Get-WmiObject Win32_Process | Where-Object { $_.GetOwner().User -eq $usuario } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }
+
+Write-Host "Todos los procesos del usuario $usuario han sido finalizados."
---
+++ unidad_8-administracion_sistemas_windows/Programador de tareas/ejercicio2.ps1
@@ -0,0 +1,7 @@
+﻿$tmp = @("C:\Windows\Temp", "$env:TEMP")
+
+foreach ($carpetas_temporales in $tmp) {
+    Get-ChildItem -Path $carpetas_temporales -Recurse -Force | Remove-Item -Force -Recurse
+}
+
+Write-Output "Limpieza de archivos temporales completada: $(Get-Date)" | Out-File -FilePath "C:\Windows\Logs\LimpiezaTemp.log"
