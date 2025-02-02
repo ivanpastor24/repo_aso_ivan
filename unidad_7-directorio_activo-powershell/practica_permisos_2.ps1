bd72fe36ab61ac978a1bc04d98e1e355271b52f0
ivanpastor24 <ipastorprats@gmail.com>
2025-02-02 21:49:00 +0100

    Ejercicio powershell modificado

--- unidad_7-directorio_activo-powershell/practica_permisos_2.ps1
+++ unidad_7-directorio_activo-powershell/practica_permisos_2.ps1
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿New-Item -Path 'C:\Empresa' -ItemType Directory
+New-Item -Path 'C:\Empresa' -ItemType Directory
 
 $departamentos = Import-Csv 'C:\Users\Administrador\Desktop\repo_aso_ivan\unidad_7-directorio_activo-powershell\archivos\departamentos.csv' -Delimiter ";"
 
@@ -39,45 +38,4 @@ foreach ($dep in $departamentos) {
  $ace_permisos_escritura | Format-Table
 
  $acl | Set-Acl -Path C:\Empresa\"$($dep.departamento)"
-=======
-﻿$ruta = "C:\Empresa"
-
-$departamentos = "C:\Empresa\FINANZAS"
-$departamentos = "C:\Empresa\PERSONAL"
-$departamentos = "C:\Empresa\PRODUCCION"
-$departamentos = "C:\Empresa\VENTAS"
-
-New-Item -Path "$ruta" -ItemType Directory
-
-New-Item -Path "$ruta\$departamentos" -ItemType Directory
-
-New-SmbShare -Path "$ruta" -Name 'Empresa' -FullAccess Administradores -ReadAccess Todos
-
-foreach ($departamento in $departamentos) {
-   $rutadep = "$ruta\$departamento"
-   
-   Write-Host $rutadep
-   
-   $acl_departamento = Get-Acl "$rutadep"
-
-   $acl_departamento.SetAccessRuleProtection($true,$false)
-
-   $permiso_modificar = @("$rutadep", 'Modify', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
-
-   $ace_permiso_modificar = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso_modificar
-
-   $permiso_lectura = @('Todos', 'Read', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
-
-   $ace_permiso_lectura = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso_lectura
-
-   $permiso_control_total = @('Administradores', 'FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
-
-   $ace_permiso_control_total = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso_control_total
-
-   $acl.SetAccessRule($ace_permiso_modificar)
-   $acl.SetAccessRule($ace_permiso_lectura)
-   $acl.SetAccessRule($ace_permiso_control_total)
-
-   $acl_departamento | Set-Acl -Path "$ruta"
->>>>>>> 7a3b04bbf3137714974b1ed71fd4df40d48e6862
-}
+ }
